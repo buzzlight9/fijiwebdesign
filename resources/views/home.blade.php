b656a5f17ba89bf4405c228e47373bcecafa67f7
Laitia Matagavui <lcavukali@yahoo.com>
2018-06-15 19:38:02 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fiji Web Solution</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link href="/css/style.css" rel="stylesheet" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+		<link rel="icon" href="bootstrap4/favicon.ico">
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="#home" class="navbar-brand"><img src="img/logo2.png" alt=""></a>
+				</div>
+				<div class="collapse navbar-collapse" id="navbar-collapse-main">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#home" class="active">HOME</a></li>
+						<li><a href="#about">ABOUT</a></li>
+						<li><a href="#services">SERVICES</a></li>
+						<li><a href="#portfolio">PORTFOLIO</a></li>
+						<li><a href="#pricing">PRICING</a></li>
+						<li><a href="#contact">CONTACT</a></li>
+					</ul>	
+				</div>
+			</div>
+		</nav>
+		<div id="home">
+			<div class="landing-text">
+				<h1>FIJI WEB SOLUTION</h1>
+				<h3>Get your bussiness online</h3>
+				<a href="#contact" class="btn btn-default btn-lg">Get Started</a>
+			</div>
+		</div>
+		<div id="about" class="padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<img src="img/responsive1.png" alt="">
+					</div>
+					<div class="col-sm-6 text-justify">
+						<h3>OUR MISSION</h3>
+						<p>Responsive web design is a type of web design that provides a customized viewing experience for different browser platforms. A website created with responsive design will display a different interface depending on what device is used to access the site. For example, a responsive website may appear one way on a laptop, another way on a tablet, and still another way on smartphone.</p>
+						<p>Today, many people access websites from mobile devices, rather than desktop computers or laptops. While most smartphones can display regular websites, the content is difficult to read and even harder to navigate. Therefore, at Fiji Web Solution we use responsive web design to provide a better web browsing experience on your website.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<div id="services" class="padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4">
+						<h3>WEB DESIGN</h3>
+						<p>Here at Fiji Web Solution, we strategically design your website with the future in mind. We use some of the latest modern technologies to bring your ideas to life. Modern...Flexible and User Friendly design to give your website an edge to compete in these digital revolution. Your input on how your website is to be design, is critical to our Planning Process.</p>
+					</div>
+					<div class="col-lg-4">
+						<h3>WEB DEVELOPMENT</h3>
+						<p>From Researching...Planning and pushing towards  the Deloyment/Launch of your new website. We liase with you throughout the Development Process. From Static Single Page websites, E-Commerce websites and Content Management Systems or CMS. A principle benefit of a CMS is that it allows you to make changes without having technical knowledge.</p>
+					</div>
+					<div class="col-lg-4">
+						<h3>RESPONSIVE DESIGN</h3>
+						<p>Today, many people access websites from mobile devices, rather than desktop computers or laptops. While most smartphones can display regular websites, the content is difficult to read and even harder to navigate. Therefore, at Fiji Web Solution we use responsive web design to provide a better web browsing experience on your website.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-6 colxs-12">
+						<h4>Design with latest technology</h4>
+						<p>Latest Adobe Products such as Adobe XD...Adobe PhotoShop. Adobe XD provides faster prototype. Adobe Illustrator for smooth and flexible vectors. Collaborating tools to keep you updated on the design. </p>	
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 colxs-12">
+						<img src="img/adobe1.png" class="img-responsive" alt="">
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 colxs-12">
+						<h4>Built with latest technology</h4>
+						<p>HTML5 and CSS3 is the skeleton for the Front-End. The Bootstrap Framework is also in-coporated. PHP drives the Back-End for a more dynamic outlook. JavaScript...Laravel are also some of the technologies we cultivate.</p>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 colxs-12">
+						<img src="img/code1.png" class="img-responsive" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--<div id="fixed"></div>-->
+		<div id="portfolio" class="padding">
+			<div class="container">
+				<div class="gallery">
+					<div class="row mb-3">
+						<div class="col-md-4">
+							<div class="">
+								<a class="lightbox" href="img/redblog.png">
+									<img src="img/redblog.png" alt="Park" class="card-img-top">
+								</a>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="">
+								<a class="lightbox" href="img/wedesign.png">
+									<img src="img/wedesign.png" alt="Park" class="card-img-top">
+								</a>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="">
+								<a class="lightbox" href="img/bridge.jpg">
+									<img src="img/bridge.jpg" alt="Park" class="card-img-top">
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="row"> 
+						<div class="col-md-4">
+							<div class="">
+								<a class="lightbox" href="img/coast.jpg">
+									<img src="img/coast.jpg" alt="Park" class="card-img-top">
+								</a>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="">
+								<a class="lightbox" href="img/rails.jpg">
+									<img src="img/rails.jpg" alt="Park" class="card-img-top">
+								</a>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="">
+								<a class="lightbox" href="img/rocks.jpg">
+									<img src="img/rocks.jpg" alt="Park" class="card-img-top">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+		<div id="pricing" class="padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="card text-center">
+							<div class="title">
+								<i class="fa fa-paper-plane" aria-hidden="true"></i>
+								<h2>Basic</h2>
+							</div>
+							<div class="price">
+								<h4><sup>£</sup>50</h4>
+							</div>
+							<div class="option">
+								<ul>
+									<li><i class="fa fa-check" aria-hidden="true"></i>Single Page</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>1 Year Free Domain</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>1 Year Hosting</li>
+									<li><i class="fa fa-times" aria-hidden="true"></i>Live Support</li>
+								</ul>
+							</div>
+							<a href="#">Order Now</a>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="card text-center">
+							<div class="title">
+								<i class="fa fa-plane" aria-hidden="true"></i>
+								<h2>Standard</h2>
+							</div>
+							<div class="price">
+								<h4><sup>£</sup>150</h4>
+							</div>
+							<div class="option">
+								<ul>
+									<li><i class="fa fa-check" aria-hidden="true"></i>Multi Page</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>1 Year Free Domain</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>1 Year Free Hosting</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>Live Support</li>
+								</ul>
+							</div>
+							<a href="#">Order Now</a>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="card text-center">
+							<div class="title">
+								<i class="fa fa-rocket" aria-hidden="true"></i>
+								<h2>Premium</h2>
+							</div>
+							<div class="price">
+								<h4><sup>£</sup>250</h4>
+							</div>
+							<div class="option">
+								<ul>
+									<li><i class="fa fa-check" aria-hidden="true"></i>Dynamic Website</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>Content Management System</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>1 Year Free Domain</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>1 Year Free Hosting</li>
+									<li><i class="fa fa-check" aria-hidden="true"></i>Live Support</li>
+								</ul>
+							</div>
+							<a href="#">Order Now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="contact" class="padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<form id="contact-form" method="post" action="contact.php" role="form">
+							<div class="messages"></div>
+							<div class="controls">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="form_name">Firstname *</label>
+											<input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
+											<div class="help-block with-errors"></div>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="form_lastname">Lastname *</label>
+											<input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required.">
+											<div class="help-block with-errors"></div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="form_email">Email *</label>
+											<input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+											<div class="help-block with-errors"></div>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="form_phone">Phone</label>
+											<input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+											<div class="help-block with-errors"></div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="form_message">Message *</label>
+											<textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+											<div class="help-block with-errors"></div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<input type="submit" class="btn btn-success btn-send" value="Send message">
+									</div>
+								</div>
+							</div>
+						</form>		
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer id="contact" class="container-fluid text-center">
+			<div class="row">
+				<div class="col-sm-4">
+					<h3>Connect</h3>
+					<a href="#" class="fa fa-facebook"></a>
+					<a href="#" class="fa fa-twitter"></a>
+					<a href="#" class="fa fa-google"></a>
+					<a href="#" class="fa fa-linkedin"></a>
+					<a href="#" class="fa fa-youtube"></a>
+				</div>
+				<div class="col-sm-4">
+					<h4>FijiWebDesign Copyright &copy; 2018</h4>	
+				</div>
+				<div class="col-sm-4">
+					<img src="img/logo3.png" class="icon" alt="">
+				</div>
+			</div>
+		</footer>
+		<script>
+			$('a[href*="#"]')
+			  // Remove links that don't actually link to anything
+			  .not('[href="#"]')
+			  .not('[href="#0"]')
+			  .click(function(event) {
+				// On-page links
+				if (
+				  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+				  && 
+				  location.hostname == this.hostname
+				) {
+				  // Figure out element to scroll to
+				  var target = $(this.hash);
+				  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				  // Does a scroll target exist?
+				  if (target.length) {
+					// Only prevent default if animation is actually gonna happen
+					event.preventDefault();
+					$('html, body').animate({
+					  scrollTop: target.offset().top
+					}, 1000, function() {
+					  // Callback after animation
+					  // Must change focus!
+					  var $target = $(target);
+					  $target.focus();
+					  if ($target.is(":focus")) { // Checking if the target was focused
+						return false;
+					  } else {
+						$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+						$target.focus(); // Set focus again
+					  };
+					});
+				  }
+				}
+			  });
+		</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+		<script>
+			baguetteBox.run('.gallery');
+		</script>
+	</body>
+</html>
